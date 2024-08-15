08eb5edbb8c2d02d861dca317f45a96ad355adb6
Kiran Prem <111084800+Isle-of-Man@users.noreply.github.com>
2024-08-15 14:05:13 +0530

    Time: 839 ms (62.05%), Space: 0B (100.00%) - LeetHub

---
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+SELECT 
+    st.student_id, 
+    st.student_name, 
+    s.subject_name, 
+    COALESCE(COUNT(e.subject_name), 0) AS attended_exams
+FROM 
+    Students st
+CROSS JOIN 
+    Subjects s
+LEFT JOIN 
+    Examinations e
+    ON st.student_id = e.student_id AND s.subject_name = e.subject_name
+GROUP BY 
+    st.student_id, 
+    st.student_name, 
+    s.subject_name
+ORDER BY 
+    st.student_id, 
+    s.subject_name;
+
+
