a7ee04b903628872429814624ae606a96a8999ef
Kiran Prem <111084800+Isle-of-Man@users.noreply.github.com>
2024-08-17 01:07:56 +0530

    Time: 777 ms (17.21%), Space: 0B (100.00%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,20 +1,24 @@
 # Write your MySQL query statement below
 
     
-select round(avg(order_date = customer_pref_delivery_date)*100,2) as    immediate_percentage 
-from Delivery
-where (customer_id , order_date) in 
-(      
-    select customer_id , min(order_date) 
-    from Delivery 
-    group by customer_id     
-) 
-
-
-# Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+# select round(avg(order_date = customer_pref_delivery_date)*100,2) as    immediate_percentage 
 # from Delivery
-# where (customer_id, order_date) in (
-#   Select customer_id, min(order_date) 
-#   from Delivery
-#   group by customer_id
-# );
+# where (customer_id , order_date) in 
+# (      
+#     select customer_id , min(order_date) 
+#     from Delivery 
+#     group by customer_id     
+# ) 
+
+with cte AS
+(
+    select customer_id , min(order_date) as earliest_order_date
+    from Delivery 
+    group by customer_id  
+)
+
+select round(avg(d.order_date = d.customer_pref_delivery_date)*100,2) as    immediate_percentage 
+from Delivery d
+JOIN cte c
+ON d.customer_id = c.customer_id
+AND d.order_date = c.earliest_order_date
